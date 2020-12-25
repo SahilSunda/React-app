13a5a62126cc661fe3112a1fd2a03c6d1760404a
Sahil <sahilsundaiit@gmail.com>
2020-12-25 18:37:25 +0530

    c

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-Parser');
 const {randomBytes} = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
 app.get('/posts', (req, res) => {
   res.send(posts);
 });
- app.post('/posts', (req, res) => {
+
+app.post('/posts', (req, res) => {
   const id = randomBytes(4).toString('hex');
   const {title} = req.body;
   
@@ -19,8 +22,8 @@ app.get('/posts', (req, res) => {
   };
   
   res.status(201).send(posts[id]);
- });
+});
 
- app.listen(4000, () => {
+app.listen(4000, () => {
   console.log('Listening on 4000');
- });
+});
