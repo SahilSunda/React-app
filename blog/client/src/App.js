d4a1c5aba56ae0dc0e3c04e6dcd37daff26fe52a
Sahil <sahilsundaiit@gmail.com>
2020-12-30 11:19:37 +0530

    restarting project

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import PostCreate from './PostCreate';
-import PostList from './PostCreate';
 
 export default () => {
-  return <div className="container">
-    <h1>Create Post</h1>
-    <PostCreate />  
-    <hr />
-    <h1>Posts</h1>
-    <PostList />
-  </div>;
+  return <div>Blog app</div>;
 };
--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -1,32 +1,8 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React from 'react';
 
 export default () => {
-  const [title, setTitle] = useState('');
-
-  const onSubmit = async event => {
-    event.preventDefault();
-
-    await axios.post('http://localhost:4000/posts', {
-      title
-    });
-
-    setTitle('');
-  };
-
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label>Title</label>
-          <input
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <button className="btn btn-primary">Submit</button>
-      </form>
-    </div>
-  );
-};
+  return <div>
+    <form>
+      <div className="form-group">
+        <> 
+}
